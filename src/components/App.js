86ee589a28a0d5b556ce3d8b447649821c82c6ec
rambabu1458 <ramu.y95@gmail.com>
2025-05-17 12:06:35 +0530

    Button Clicked done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[count, setCount]=useState(0);
+   const handleClickInc = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+   const handleClickDec = () => {
+    setCount(prevCount =>  (prevCount > 0 ? prevCount - 1 : 0));
+  };
   return (
     <div>
         {/* Do not remove the main div */}
+        <h2>Button clicked {count} {count === 0 ? "time" : 'times'}</h2>
+        <button onClick={handleClickInc}>Click me Increment</button>
+        <button onClick={handleClickDec}>Click me Decrement</button>
     </div>
   )
 }
